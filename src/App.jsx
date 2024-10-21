74fcadbdb1f33eb096d22a9178a00764762ee57b
zahid-webb <zahiduzzaman064@gmail.com>
2024-10-21 15:10:17 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+   <h1>First React Code</h1> 
+   <marquee> Error!!404!!</marquee>
+   <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
